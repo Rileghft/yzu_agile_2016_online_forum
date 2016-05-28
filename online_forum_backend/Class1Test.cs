1bc6b8e27d24bdd0405d7e382bdba839a2affabb
askalee <aska.lee@gmail.com>
2016-05-28 12:15:14 +0800

    add sample test

---
+++ online_forum_backend/Class1Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_forum_backend
+{
+    [TestFixture]
+    public class Class1Test
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(1 + 1, Is.EqualTo(2));
+        }
+    }
+}
