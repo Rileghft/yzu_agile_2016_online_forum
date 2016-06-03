eaa7145f4bb04c458535b1a6d5763c497314b011
rita20839 <rita20839@gmail.com>
2016-06-03 21:57:36 +0800

    Register

---
+++ online_forum_backend/Action.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_forum_backend
+{
+
+    class Action
+    {
+        public bool register(string account, string password)
+        {
+            ForumDB db = new ForumDB();
+            db.insertUser(account, password);
+            bool result = db.insertUser(account, password);
+            if (result==true)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ online_forum_backend/ActionTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_forum_backend
+{
+    [TestFixture]
+    class ActionTest
+    {
+        [Test]
+        public void 註冊新用戶()
+        {
+            ForumDB db = new ForumDB();
+            Action Register = new Action();
+            Register.register("newaccount", "newpass");
+            bool result = Register.register("newaccount", "newpass");
+            Assert.That(false, Is.EqualTo(result));
+            //Assert.That("newaccount", Is.EqualTo(db.accounts[1].name));
+            //Assert.That("newpass", Is.EqualTo(db.accounts[1].password));
+        }
+    }
+}
