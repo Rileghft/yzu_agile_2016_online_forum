f1789429d674080bab0a86eb4bc94cf6db5923e9
wuyanxian <826593532@qq.com>
2016-06-03 23:27:42 +0800

    add register function

---
+++ online_forum_backend/register.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_forum_backend
+{
+    class register
+    {
+        public register(string userName, string passwd)
+        {
+            Console.Write("Enter your userName:");
+            userName = Console.ReadLine();
+            if (userName == null)
+            {
+                Console.Write("Enter your password:");
+                passwd = Console.ReadLine();
+
+                Console.Write("Enter your password again:");
+                string passwd2 = Console.ReadLine();
+
+                if (passwd == passwd2)
+                    Console.Write("Register success!");
+                else
+                    Console.Write("Incorrect password!");
+            }
+            else
+                Console.Write("The account already exist!");
+        }
+    }
+}
